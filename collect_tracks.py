83cb91868f3661e59026e901d999ea6c9406f17d
Noémie <noemie.lehuby@zaclys.net>
2019-08-29 10:17:55 +0200

    init

---
+++ collect_tracks.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+### nlehuby - AbidjanTransport
+
+from gpx_snippets import collect_tracks_from_osm
+
+collect_tracks_from_osm.download_all_tracks_from_tag("abidjantransport", "abidjan/")
