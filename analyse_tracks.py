dba03a04337a829462393d88adc8e8cb16f302e7
Noémie <noemie.lehuby@zaclys.net>
2019-10-15 09:04:24 +0200

    analyse stop and get mode

--- analyse_tracks.py
+++ analyse_tracks.py
@@ -8,9 +8,16 @@ import os
 path = "abidjan/"
 dirs = os.listdir( path )
 
-stop_waypoints = ["abribus", "arrêt sans indication","poteau"] #TODO (this list is non exhaustive)
+stop_waypoints = ["abribus", "arrêt sans indication","poteau", "vide", "mi-plein", "plein", "surchargé"]
 
 result = []
+
+def get_mode(track_name):
+    if "gbaka" in track_name.lower():
+        return "gbaka"
+    if "w_r_" in track_name.lower():
+        return "woro woro"
+    return filename.split('_')[0]
 
 for filename in dirs:
     if not filename.endswith(".gpx"):
@@ -22,9 +29,10 @@ for filename in dirs:
         elem["duration"] = round(gpx.get_duration()/60)
         elem["average_speed"] = round((gpx.length_2d() / 1000) / (gpx.get_duration() / 3600))
         explode = filename.split('_')
-        elem["line"] = explode[0]
+        elem["mode"] = get_mode(filename)
         elem["direction"] = explode[1]
         elem["gpx"] = filename
+
 
         arrets = [elem for elem in gpx.waypoints if elem.name in stop_waypoints]
         elem["stop_number"] = len(arrets)
@@ -32,7 +40,7 @@ for filename in dirs:
 
     result.append(elem)
 
-headers = ["gpx", "line", "direction", "duration", "distance", "average_speed", "stop_number" ,"other_meta_number"]
+headers = ["mode", "direction", "duration", "distance", "average_speed", "stop_number" ,"other_meta_number", "gpx"]
 with open("abidjan/analyse_gpx.csv", 'w') as myfile:
     wr = csv.DictWriter(myfile, quoting=csv.QUOTE_ALL, fieldnames = headers)
     wr.writeheader()
