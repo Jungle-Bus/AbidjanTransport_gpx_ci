6a18446c80f8f0a22197e5366c2347ce9175da9b
Noémie <noemie.lehuby@zaclys.net>
2019-10-17 11:51:20 +0200

    add metadata from osm.org

--- analyse_tracks.py
+++ analyse_tracks.py
@@ -6,12 +6,8 @@ import csv
 import os
 import datetime
 
-path = "abidjan/"
-dirs = os.listdir( path )
+from gpx_snippets import collect_tracks_from_osm
 
-stop_waypoints = ["abribus", "arrêt sans indication","poteau", "vide", "mi-plein", "plein", "surchargé"]
-
-result = []
 
 def get_mode_or_line(track_name):
     if "gbaka" in track_name.lower():
@@ -36,34 +32,43 @@ def get_is_peak_hour(gpx_time):
             return True
     return False
 
-for filename in dirs:
-    if not filename.endswith(".gpx"):
-        continue
-    elem = {}
-    with open(path + filename, "r") as gpx_file:
-        gpx = gpxpy.parse(gpx_file)
-        elem["distance"] = round(gpx.length_2d())
-        elem["duration"] = round(gpx.get_duration()/60)
-        elem["average_speed"] = round((gpx.length_2d() / 1000) / (gpx.get_duration() / 3600))
-        elem["line"] = get_mode_or_line(filename)
-        elem["direction"] = get_direction(filename)
-        elem["gpx"] = filename
 
-        start_time, _ = gpx.get_time_bounds()
-        elem["date"] = start_time.strftime('%Y-%m-%d')
-        elem["heure pointe"] = (get_is_peak_hour(start_time.time()))
+if __name__ == '__main__':
 
-        arrets = [elem for elem in gpx.waypoints if elem.name in stop_waypoints]
-        elem["stop_number"] = len(arrets)
-        elem["other_meta_number"] = len(gpx.waypoints) - len(arrets)
+    path = "abidjan/"
 
-    result.append(elem)
+    all_tracks = collect_tracks_from_osm.download_all_tracks_from_tag("abidjantransport",  path)
 
-result = sorted(result, key=lambda k: k['date'], reverse=True)
+    result = []
 
-headers = ["line", "direction", "duration", "distance", "average_speed", "stop_number" ,"other_meta_number", "date", "heure pointe", "gpx"]
-with open("abidjan/analyse_gpx.csv", 'w') as myfile:
-    wr = csv.DictWriter(myfile, quoting=csv.QUOTE_ALL, fieldnames = headers)
-    wr.writeheader()
-    for a_row in result :
-        wr.writerow(a_row)
+    stop_waypoints = ["abribus", "arrêt sans indication","poteau", "vide", "mi-plein", "plein", "surchargé"]
+
+    for elem in all_tracks:
+        filename = elem["name"]
+        with open(path + filename, "r") as gpx_file:
+            gpx = gpxpy.parse(gpx_file)
+            elem["distance"] = round(gpx.length_2d())
+            elem["duration"] = round(gpx.get_duration()/60)
+            elem["average_speed"] = round((gpx.length_2d() / 1000) / (gpx.get_duration() / 3600))
+            elem["line"] = get_mode_or_line(filename)
+            elem["direction"] = get_direction(filename)
+
+            start_time, _ = gpx.get_time_bounds()
+            elem["date"] = start_time.strftime('%Y-%m-%d')
+            elem["heure pointe"] = (get_is_peak_hour(start_time.time()))
+
+            arrets = [elem for elem in gpx.waypoints if elem.name in stop_waypoints]
+            elem["stop_number"] = len(arrets)
+            elem["other_meta_number"] = len(gpx.waypoints) - len(arrets)
+
+        result.append(elem)
+
+    result = sorted(result, key=lambda k: k['date'], reverse=True)
+
+    headers = ["line", "direction", "duration", "distance", "average_speed", "stop_number" ,"other_meta_number", "date", "user", "heure pointe", "name", "trace_id"]
+    result_to_persist = [ dict((k, el.get(k, None)) for k in headers) for el in result]
+    with open("abidjan/analyse_gpx.csv", 'w') as myfile:
+        wr = csv.DictWriter(myfile, quoting=csv.QUOTE_ALL, fieldnames = headers)
+        wr.writeheader()
+        for a_row in result_to_persist :
+            wr.writerow(a_row)
